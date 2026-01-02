ec7b909873329761f33a131ea873ce3f97d0e24d
HimAnshu165-sudo <himanshushar213@gmail.com>
2026-01-02 15:50:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-/**
- * @format
- */
+const express = require("express");
+const app = express();
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-AppRegistry.registerComponent(appName, () => App);
+app.get("/user", (req, res) => {
+  res.json([
+    {
+      id:1,
+      name: "Amit",
+      role: "Frontend Dev",
+      salary: 15000,
+    },
+    {
+      id:2,
+      name: "Hemu",
+      role: "Frontend Dev",
+      salary: 18000,
+    },
+    {
+      id:3,
+      name: "Himanshu",
+      role: "Frontend Dev",
+      salary: 20000,
+    }
+  ]);
+});
+app.get("/client", (req, res) => {
+  res.json([
+    {
+      id:1,
+      name: "Salanki",
+      role: "Api User",
+    },
+    {
+      id:2,
+      name: "xyz",
+      role: "Api user",
+    }
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
